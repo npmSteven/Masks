dd2b3e2399747cd50079300cc17e2cf6301eaebb
BuildTools <unconfigured@null.spigotmc.org>
2020-04-15 16:42:18 +0100

    Initial commit with basic plugin setup

---
+++ src/net/stevenrafferty/headhunting/Main.java
@@ -0,0 +1,16 @@
+package net.stevenrafferty.headhunting;
+
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    public void onEnable() {
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "HeadHunting has been Enabled");
+    }
+
+    public void onDisable() {
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "HeadHunting has been Disabled");
+    }
+
+}
