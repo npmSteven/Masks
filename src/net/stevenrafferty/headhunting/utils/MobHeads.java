66da615a9af780a185168dba199467847c59ed3f
BuildTools <unconfigured@null.spigotmc.org>
2020-04-17 17:54:07 +0100

    Updated config.yml with mob data, add mob_head_drops to permissions

--- src/net/stevenrafferty/headhunting/utils/MobHeads.java
+++ src/net/stevenrafferty/headhunting/utils/MobHeads.java
@@ -8,19 +8,10 @@ public class MobHeads {
     boolean toReturn = false;
     switch (type) {
       case BLAZE:
-      case CAVE_SPIDER:
-      case CHICKEN:
       case COW:
-      case CREEPER:
-      case ENDERMAN:
-      case GHAST:
       case IRON_GOLEM:
       case PIG:
-      case SHEEP:
-      case MUSHROOM_COW:
       case SKELETON:
-      case SLIME:
-      case SPIDER:
       case ZOMBIE: {
         toReturn = true;
         break;
@@ -36,28 +27,8 @@ public class MobHeads {
         toReturn = "MHF_Blaze";
         break;
       }
-      case CAVE_SPIDER: {
-        toReturn = "MHF_CaveSpider";
-        break;
-      }
-      case CHICKEN: {
-        toReturn = "MHF_Chicken";
-        break;
-      }
       case COW: {
         toReturn = "MHF_Cow";
-        break;
-      }
-      case CREEPER: {
-        toReturn = "MHF_Creeper";
-        break;
-      }
-      case ENDERMAN: {
-        toReturn = "MHF_Enderman";
-        break;
-      }
-      case GHAST: {
-        toReturn = "MHF_Ghast";
         break;
       }
       case IRON_GOLEM: {
@@ -68,24 +39,8 @@ public class MobHeads {
         toReturn = "MHF_Pig";
         break;
       }
-      case SHEEP: {
-        toReturn = "MHF_Sheep";
-        break;
-      }
-      case MUSHROOM_COW: {
-        toReturn = "MHF_MushroomCow";
-        break;
-      }
       case SKELETON: {
         toReturn = "MHF_Skeleton";
-        break;
-      }
-      case SLIME: {
-        toReturn = "MHF_Slime";
-        break;
-      }
-      case SPIDER: {
-        toReturn = "MHF_Spider";
         break;
       }
       case ZOMBIE: {
