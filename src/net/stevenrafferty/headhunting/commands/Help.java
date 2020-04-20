dda15bec190c9f1cfa03e36725a43847d41d5b22
BuildTools <unconfigured@null.spigotmc.org>
2020-04-20 15:07:05 +0100

    Removed table option

--- src/net/stevenrafferty/headhunting/commands/Help.java
+++ src/net/stevenrafferty/headhunting/commands/Help.java
@@ -1,12 +1,10 @@
 package net.stevenrafferty.headhunting.commands;
 
-import net.stevenrafferty.headhunting.Main;
 import net.stevenrafferty.headhunting.utils.Helper;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 
 public class Help implements CommandExecutor {
 
