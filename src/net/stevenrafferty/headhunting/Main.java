5e546dee4b37d266127b08120d261bafb3863ecd
Steven <28809905+StevenRafferty@users.noreply.github.com>
2020-07-18 21:44:36 +0100

    Revert "Revert 20 develop"

--- src/net/stevenrafferty/headhunting/Main.java
+++ src/net/stevenrafferty/headhunting/Main.java
@@ -1,16 +1,90 @@
 package net.stevenrafferty.headhunting;
 
+import net.milkbowl.vault.economy.Economy;
+import net.stevenrafferty.headhunting.commands.*;
+import net.stevenrafferty.headhunting.events.*;
+import net.stevenrafferty.headhunting.utils.Database;
+import net.stevenrafferty.headhunting.utils.Helper;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.sql.SQLException;
 
 public class Main extends JavaPlugin {
 
-    public void onEnable() {
-        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "HeadHunting has been Enabled");
+  private static Economy econ = null;
+
+  @Override
+  public void onEnable() {
+    // Listeners
+    getServer().getPluginManager().registerEvents(new MobHeadDrop(), this);
+    getServer().getPluginManager().registerEvents(new TokensInventory(), this);
+    getServer().getPluginManager().registerEvents(new PlayerKill(), this);
+    getServer().getPluginManager().registerEvents(new MaskInventory(), this);
+    getServer().getPluginManager().registerEvents(new DisableHeadPlace(), this);
+    getServer().getPluginManager().registerEvents(new MaskEffects(), this);
+    getServer().getPluginManager().registerEvents(new UpgradeInventory(), this);
+
+    // Commands
+    getCommand("redeem").setExecutor(new Redeem());
+    getCommand("souls").setExecutor(new Souls());
+    getCommand("headhunting").setExecutor(new Help());
+    getCommand("masks").setExecutor(new Mask());
+    getCommand("upgrade").setExecutor(new Upgrade());
+
+    getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "HeadHunting: Enabled");
+    loadConfig();
+    removeMaterials();
+
+    // Connect to db
+    Database database = new Database();
+
+    try {
+      database.connect();
+    } catch (ClassNotFoundException error) {
+      error.printStackTrace();
+    } catch (SQLException error) {
+      error.printStackTrace();
     }
 
-    public void onDisable() {
-        getServer().getConsoleSender().sendMessage(ChatColor.RED + "HeadHunting has been Disabled");
+    if (!setupEconomy()) {
+      System.out.println("No economy plugin found. Disabling Vault");
+      getServer().getPluginManager().disablePlugin(this);
     }
+
+  }
+
+  @Override
+  public void onDisable() {
+    getServer().getConsoleSender().sendMessage(ChatColor.RED + "HeadHunting: Disabled");
+  }
+
+  public void loadConfig() {
+    getConfig().options().copyDefaults(true);
+    saveConfig();
+  }
+
+  public void removeMaterials() {
+    Helper helper = new Helper();
+    helper.remove(Material.BEACON);
+  }
+
+  private boolean setupEconomy() {
+    if (getServer().getPluginManager().getPlugin("Vault") == null) {
+      return false;
+    }
+    RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+    if (rsp == null) {
+      return false;
+    }
+    econ = rsp.getProvider();
+    return econ != null;
+  }
+
+  public static Economy getEconomy() {
+    return econ;
+  }
 
 }
---
+++ src/net/stevenrafferty/headhunting/commands/Mask.java
@@ -0,0 +1,58 @@
+package net.stevenrafferty.headhunting.commands;
+
+import net.stevenrafferty.headhunting.Main;
+import net.stevenrafferty.headhunting.utils.Helper;
+import net.stevenrafferty.headhunting.utils.ItemStacks;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+
+import java.util.Set;
+
+public class Mask implements CommandExecutor {
+
+
+    private Plugin plugin = Main.getPlugin(Main.class);
+
+    Helper helper = new Helper();
+
+    ItemStacks itemStacks = new ItemStacks();
+
+    String masksPermission = plugin.getConfig().getString("permissions.masks");
+    String noConsoleCommandMessage = helper.getConfigMessage("messages.no_console_command_message");
+    String noPermissionsMessage = helper.getConfigMessage("messages.no_permissions_message");
+
+    String maskInventoryName = helper.getConfigMessage("options.mask_inventory_name");
+
+    Set creatures = plugin.getConfig().getConfigurationSection("creatures.").getKeys(false);
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (player.hasPermission(masksPermission)) {
+                Inventory inventory = plugin.getServer().createInventory(null, 9, maskInventoryName);
+
+                int index = 0;
+                for (Object key : creatures) {
+                    String creature = key.toString();
+
+                    ItemStack token = itemStacks.maskItemStack(creature, "1", true);
+                    inventory.setItem(index, token);
+                    index++;
+                }
+
+                player.openInventory(inventory);
+            } else {
+                player.sendMessage(noPermissionsMessage);
+            }
+        } else {
+            sender.sendMessage(noConsoleCommandMessage);
+        }
+        return true;
+    }
+}
